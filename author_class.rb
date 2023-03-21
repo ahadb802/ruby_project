3647c54b3ac2575ab225280b5391b8debf802ef3
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-21 08:56:31 -0500

    Fixed the association with items & deleted add_item method

--- author_class.rb
+++ author_class.rb
@@ -1,16 +1,10 @@
 class Author
-  attr_accessor :items
+  attr_reader :items
 
   def initialize(first_name:, last_name:, items:)
     @first_name = first_name
     @last_name = last_name
     @items = items
   end
-
-  def add_item(element)
-    @items << element
-    element.item = self
-  end
 end
-
 
