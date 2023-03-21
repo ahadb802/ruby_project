fb8860de55883e295def751ee446e95f8a0bcd48
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-21 14:14:26 -0500

    Added can be archived method into Game class

--- game_class.rb
+++ game_class.rb
@@ -1,6 +1,11 @@
-class Game
+class Game < Item
   def initialize(multiplayer:, last_played_at:)
+    super()
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def can_be_archived?()
+    return false unless Item.can_be_archived? || last_played_at > 2
   end
 end
