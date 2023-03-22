b4c119375b6b4b40271434fb2000aa615311885e
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-22 10:51:14 -0500

    Added item file

---
+++ item.rb
@@ -0,0 +1,26 @@
+class Item
+  attr_accessor :publish_date
+
+  def initialize(publish_date, archived)
+    @id = Time.now.to_f.to_s.gsub('.', '')
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def years_since(date)
+    today = Time.now
+    total = today.year - date.year
+    total -= 1 if
+        date.month > today.month or
+        (date.month >= today.month and date.day > today.day)
+    total
+  end
+
+  def can_be_archived?
+    years_since(@publish_date) > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived? == true
+  end
+end
