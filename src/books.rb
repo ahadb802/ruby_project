8bfbac4350ca4737c8827a2982e2137eb4d4c902
ahadb802 <abdulahadb802@gmail.com>
2023-03-23 01:15:51 +0500

    update book class

--- src/books.rb
+++ src/books.rb
@@ -1,9 +1,9 @@
-require './item'
+require_relative '../item'
 class Book < Item
   attr_reader :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
-    super()
+  def initialize(publisher, cover_state, publish_date, archived)
+    super(publish_date, archived)
     @publisher = publisher
     @cover_state = cover_state
   end
