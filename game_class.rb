0efdda18c9808e3a84193fec0e91595810c030c5
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-22 07:48:11 -0500

    Made adjustments to game and author class

--- game_class.rb
+++ game_class.rb
@@ -1,11 +1,12 @@
 class Game < Item
-  def initialize(multiplayer:, last_played_at:)
-    super()
-    @multiplayer = multiplayer
-    @last_played_at = last_played_at
+  attr_accessor :multiplayer, :last_played_at, :publish_date
+
+  def initialize(genre, author, label, publish_date, last_played_at)
+    super(genre, author, label, publish_date, last)
+    @last_played_at = Date.parse(last_played_at)
   end
 
   def can_be_archived?()
-    return false unless Item.can_be_archived? || last_played_at > 2
+    super && ((Date.today.year - @last_played_at.year) > 2)
   end
 end
