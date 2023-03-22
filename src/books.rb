ae6305f16a08c02d752cd32f87401bb5f974d69a
ahadb802 <abdulahadb802@gmail.com>
2023-03-23 02:43:53 +0500

    update books

--- src/books.rb
+++ src/books.rb
@@ -1,4 +1,5 @@
 require_relative '../item'
+
 class Book < Item
   attr_reader :publisher, :cover_state
 
@@ -10,5 +11,14 @@ class Book < Item
 
   def can_be_archived?
     super || @cover_state == 'bad'
+  end
+
+  def to_h
+    {
+      publisher: @publisher,
+      cover_state: @cover_state,
+      publish_date: @publish_date.to_s,
+      archived: @archived
+    }
   end
 end
