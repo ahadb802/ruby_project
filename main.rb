a9333385b19a88b830de72ad55ee29ed3774f976
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-23 15:24:36 -0500

    Organize main rb file

--- main.rb
+++ main.rb
@@ -5,9 +5,9 @@ menu = Menu.new
 app = App.new
 menu.add_option('List all books', -> { app.list_all_books })
 menu.add_option('List all Labels', -> { app.list_all_labels })
-menu.add_option('Create a Book', -> { app.add_book })
 menu.add_option('List all Games', -> { app.games_list })
 menu.add_option('List all Authors', -> { app.authors_list })
+menu.add_option('Create a Book', -> { app.add_book })
 menu.add_option('Create a game', -> { app.create_game })
 menu.add_option('Quit', -> { app.exit_app })
 menu.show
