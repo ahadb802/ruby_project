4ee969e5a3fc9b7f256acc274fa72e5af726b9f2
ahadb802 <abdulahadb802@gmail.com>
2023-03-23 05:44:47 +0500

    remove comments

--- src/label.rb
+++ src/label.rb
@@ -1,17 +1,1 @@
-# require_relative 'item'
-
-# class Label
-#   attr_reader :id, :color, :title, :items
-
-#   def initialize(color, title)
-#     @id = rand(1000..9999)
-#     @color = color
-#     @title = title
-#     @items = []
-#   end
-
-#   def add_item(item)
-#     @items << item
-#     item.label = self
-#   end
-# end
+require_relative 'item'
