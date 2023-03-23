c98dc24600e69d0a1c23a50018b2f26321fb57c5
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-23 10:21:31 -0500

    Added the test to change to true

--- game.rb
+++ game.rb
@@ -2,12 +2,12 @@ require 'date'
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at, :publish_date, :archived
+  attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at)
-    super(archived, publish_date)
+  def initialize(multiplayer, last_played_at, publish_date, archived)
+    super(publish_date, archived)
     @multiplayer = multiplayer
-    @last_played_at = Date.parse(last_played_at)
+    @last_played_at = last_played_at
   end
 
   def can_be_archived?()
--- spec/author_game_spec.rb
+++ spec/author_game_spec.rb
@@ -1,3 +1,4 @@
+require 'date'
 require_relative '../author'
 require_relative '../game'
 
@@ -24,15 +25,18 @@ end
 describe Game do
   context 'Game class tests' do
     it 'Testing multiplayer name' do
-      example_game = Game.new('multiplayer_ex', '10/08/2023')
+      example_game = Game.new('multiplayer_ex', Date.new(2000, 1, 1), Date.new(2000, 1, 1), true)
       expect(example_game.multiplayer).to eql 'multiplayer_ex'
     end
 
     it 'sets archived to true when game is archived' do
-      newgame = Game.new('multiplayer_ex', '10/08/2023')
+      newgame = Game.new('multiplayer_ex', Date.new(2000, 1, 1), Date.new(2000, 1, 1), false)
       expect(newgame.instance_variable_get(:@archived)).to eq(false)
       newgame.move_to_archive
       expect(newgame.instance_variable_get(:@archived)).to eq(true)
+    end
+
+    it 'can be archived?' do
     end
   end
 end
