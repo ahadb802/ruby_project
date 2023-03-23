412215d19ff404ca53502baa0bb6d201b7048c33
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-24 02:54:49 +0600

    Fix menu items.

--- main.rb
+++ main.rb
@@ -13,7 +13,6 @@ menu.add_option('List all Games', -> { app.games_list })
 menu.add_option('List all Authors', -> { app.list_authors })
 menu.add_option('Create a Book', -> { app.add_book })
 menu.add_option('Create a Music Album', -> { app.albums.add_album })
-menu.add_option('Create a Book', -> { app.add_book })
 menu.add_option('Create a game', -> { app.create_game })
 menu.add_option('Quit', -> { app.exit_app })
 menu.show
