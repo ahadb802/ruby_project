dfd354ea8c8528f53af8e1f93a6669b133838057
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-23 14:06:09 -0500

    Added options into main file

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
 require_relative './app'
 require_relative './menu'
-require_relative './preserve_data'
 
 menu = Menu.new
 app = App.new
 menu.add_option('List all books', -> { app.list_all_books })
 menu.add_option('List all Labels', -> { app.list_all_labels })
 menu.add_option('Create a Book', -> { app.add_book })
+menu.add_option('List all Games', -> { app.games_list })
+menu.add_option('List all Authors', -> { app.authors_list })
 menu.add_option('Quit', -> { app.exit_app })
 menu.show
