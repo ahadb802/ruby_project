6d18117a5c62964d5cf69a46c2aa7e59f6902fdd
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-23 11:16:33 -0500

    Fixed linter error

--- game.rb
+++ game.rb
@@ -12,7 +12,7 @@ class Game < Item
 
   def can_be_archived?()
     today = Time.now
-    total = today.year - Date.year
+    total = today.year - Date.now
     total -= 1 if
         date.month > today.month or
         (date.month >= today.month and date.day > today.day)
